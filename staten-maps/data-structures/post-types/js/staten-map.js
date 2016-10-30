2e16bf1269e3cf0b7020e2a4df57f81363ca19c2
Mat Gargano <mgargano@gmail.com>
2016-10-29 23:18:48 -0400

    fixes for hitting enter on an address

--- staten-maps/data-structures/post-types/js/staten-map.js
+++ staten-maps/data-structures/post-types/js/staten-map.js
@@ -1,12 +1,22 @@
 jQuery(function ($) {
 
+    var $body = $('body');
+    $body.on('keypress', '.acf-field-staten-address input[type="text"]', function (e) {
 
-    $('body').on('click', '.staten-get-lat-lng', function (e) {
+        if (e.which === 13) {
+            e.preventDefault();
+
+            var $parent = $(this).parents('.acf-field-staten-address');
+            $parent.find('.staten-get-lat-lng').click();
+        }
+
+    });
+    $body.on('click', '.staten-get-lat-lng', function (e) {
         e.preventDefault();
 
 
-        var $parent = $(this).parents('.acf-repeater'),
-            $addressOuter = $(this).parents('.acf-fields').find('.acf-field-staten-address'),
+        var $parent = $(this).parents('.acf-row'),
+            $addressOuter = $parent.find('.acf-field-staten-address'),
             address = $addressOuter.find('input[type="text"]').val(),
             nonce = $addressOuter.find('.nonce').attr('data-nonce'),
             $message = $addressOuter.find('.lat-lng-message'),
